333ec73969c2d69a4860d2d6eda9960d0fa35cc7
Klaven Jones <klavensjones@gmail.com>
2020-11-09 11:27:18 -0500

    Provisioned app to Elastic Beanstalk

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,17 +1,16 @@
-
-
+require("dotenv").config()
 
 
 export const config = {
   "dev": {
-    "username": "udagramkjonesdev",
-    "password": "Basket#12",
-    "database": "udagramkjonesdev",
-    "host": "udagramkjonesdev.cyjvampmqrui.us-east-2.rds.amazonaws.com",
+    "username": process.env.POSTGRES_USERNAME,
+    "password": process.env.POSTGRES_PASSWORD,
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-klay-dev"
+    "aws_region": process.env.AWS_REGION,
+    "aws_profile": process.env.AWS_PROFILE,
+    "aws_media_bucket": process.env.AWS_BUCKET
   },
   "prod": {
     "username": "",
@@ -21,3 +20,4 @@ export const config = {
     "dialect": "postgres"
   }
 }
+
